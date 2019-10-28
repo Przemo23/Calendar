093d555052b2b762582298aa0b61b9dbb9c3f766
przemo23 <przemek.bedelek@hotmail.de>
2019-10-28 12:58:37 -0700

    Views - non -dynamic

---
+++ Controllers/CalendarController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace P01.Controllers
+{
+    public class CalendarController : Controller
+    {
+        // 
+        // GET: /Calendar/
+
+        public IActionResult Index()
+        {
+            return  View();
+        }
+        [Route("/Calendar/Edit", Name = "Edit")]
+        public IActionResult Edit()
+        {
+            return  View();
+        }
+        public IActionResult New()
+        {
+            return  View("Edit");
+        }
+
+
+
+        // 
+        // GET: /Calendar/Welcome/ 
+
+         public string Welcome()
+        {
+            return "This is the Welcome action method...";
+        }
+    }
+}
---
+++ Models/NoteModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P01.Models
+{
+    public class Note
+    {
+        public String title {get;set;}
+        public DateTime date {get;set;}
+        public String category {get;set;}
+    }
+}
